c90b573f082df90d45e0367d7c46f21f67a01f59
Abdullah-Badyah <badyah94@gmail.com>
2023-07-21 01:31:20 +0300

    Added advanced search fields (part2)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,10 +17,10 @@ class SearchController extends Controller
         $query2 = $request->input('query2');
         $query3 = $request->input('query3');
         $query4 = $request->input('query4');
+        $advancSearch = $request->input('advancSearch');
         $request = request();
 
         // return $query4Values;
-
 
         $results = Post::where('verified', 1);
 
@@ -28,50 +28,43 @@ class SearchController extends Controller
             $results->where('title', 'like', '%'.$query.'%');
         }
 
-        if ($query2) {
-            $results->whereHas('createdBy', function ($innerQuery) use ($query2) {
-                $innerQuery->where('name', 'like', '%'.$query2.'%');
-            });
-        }
-
-        if ($query3) {
-            $results->whereDate('created_at', '=', $query3);
-        }
-
-        // if ($query4) {
-        //     $results->whereHas('categories', function ($innerQuery) use ($query4) {
-        //         $innerQuery->where('name', 'like', '%'.$query4.'%');
-        //     });
-        // }
-
-        if ($query4) {
-            // $categories = explode(',', $query4);
-            $results->whereHas('categories', function ($innerQuery) use ($query4) {
-                $innerQuery->whereIn('name', $query4);
-            });
-        }
+        
+            if ($query2) {
+                $results->whereHas('createdBy', function ($innerQuery) use ($query2) {
+                    $innerQuery->where('name', 'like', '%'.$query2.'%');
+                });
+            }
+    
+            if ($query3) {
+                $results->whereDate('created_at', '=', $query3);
+            }
+    
+            if ($query4) {
+                // $categories = explode(',', $query4);
+                $results->whereHas('categories', function ($innerQuery) use ($query4) {
+                    $innerQuery->whereIn('name', $query4);
+                });
+            }
+       
         
         $results = $results->get();
+
+        $selectedValues = is_array(request('query4')) ? request('query4') : explode(',', request('query4', ''));
 
        
         $cates = Category::pluck('name', 'name');
         $users = User::pluck('name', 'name');
         $posts = Post::where('verified', 1)->latest()->paginate(2); //For footer layout - latest posts widget
 
-        $q4Array = [];
-        $match = preg_match('/query4=(.*?)&/', $request, $matches);
-        // if ($match) {
-        //     $q4Array = explode('&', $matches[1]);
-        // }
-        $q4Array = $matches;
+        // $q4Array = [];
+        // $match = preg_match('/query4=(.*?)&/', $request, $matches);
+        // // if ($match) {
+        // //     $q4Array = explode('&', $matches[1]);
+        // // }
+        // $q4Array = $matches;
 
-        return view('posts.search', compact('results', 'posts', 'users', 'cates', 'query', 'query2', 'query3', 'query4', 'request', 'q4Array'));
+        return view('posts.search', compact('results', 'posts', 'users', 'cates', 'query', 'query2', 'query3', 'query4','selectedValues'));
       
     }
 
-    // public function index()
-    // {
-    //     $users = User::all();
-    //     return view('posts.search', compact('users'));
-    // }
 }
--- resources/views/posts/search.blade.php
+++ resources/views/posts/search.blade.php
@@ -1,63 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-5">
-<div class="row">
-    <div class="col-3"></div>
-    <div class="col-6">
-        <div class="search">
-            <form action="{{ route('search') }}" method="GET">
-                <i class="fa fa-search"></i>
-                <input type="text" name="query" class="form-control" value="{{$query}}" placeholder="Type here to search...">
-                <!-- <input type="text" name="query2" class="form-control" placeholder="Author..."> -->
-                
-                <!-- <select class="fstdropdown-select" id="example" value="{{$query2}}" name="query2">
-                    <option value="">Select option</option>
-                    @foreach ($users as $name)
-                        <option value="{{ $name }}" {{ $query2 == $name ? 'selected' : '' }}>{{ $name }}</option>
-                    @endforeach
-                </select> -->
+<div class="container py-5"> 
 
-                <select class="js-example-basic-single" value="{{$query2}}" name="query2">
-                    <option value="">Select option</option>
-                    @foreach ($users as $name)
-                        <option value="{{ $name }}" {{ $query2 == $name ? 'selected' : '' }}>{{ $name }}</option>
-                    @endforeach
-                </select>
+    <form action="{{ route('search') }}" method="GET">
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-8 pe-1">
+                        <label class="search-lbl">Title</label>
+                        <input type="text" name="query" class="form-control" value="{{$query}}">
+                    </div>
+                    <div class="col-4">
+                        <label class="search-lbl">Date</label>
+                        <input name="query3" id="startDate" class="form-control" value="{{$query3}}" type="date"/>
+                    </div>
+                </div>
 
-                <!-- <select class="js-example-basic-multiple" name="query4"  multiple="multiple">
-                @foreach ($cates as $name)
-                    <option value="{{ $name }}">{{ $name }}</option>
-                @endforeach
-                </select> -->
-                
-                
-                @php
-                    $selectedValues = is_array(request('query4')) ? request('query4') : explode(',', request('query4', ''));
-                @endphp
-
-                <select class="js-example-basic-multiple" name="query4[]" multiple="multiple">
-                    @foreach ($cates as $name)
-                        <option value="{{ $name }}" {{ in_array($name, $selectedValues) ? 'selected' : '' }}>{{ $name }}</option>
-                    @endforeach
-                </select>
-
-                <input name="query3" id="startDate" class="form-control" value="{{$query3}}" type="date"/>
-
+                <div class="row mb-2">
+                    <div class="col-8 pe-1">
+                        <label class="search-lbl">Categories</label>
+                        <select class="js-example-basic-multiple form-control" name="query4[]" multiple="multiple">
+                            <option value="">Categories</option>
+                            @foreach ($cates as $name)
+                                <option value="{{ $name }}" {{ in_array($name, $selectedValues) ? 'selected' : '' }}>{{ $name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-4">
+                        <label class="search-lbl">Author</label>
+                        <select class="js-example-basic-single" value="{{$query2}}" name="query2">
+                            <option value=""></option>
+                            @foreach ($users as $name)
+                                <option value="{{ $name }}" {{ $query2 == $name ? 'selected' : '' }}>{{ $name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
                 
                 
-                <button type="submit" class="btn btn-primary">Search</button>
-            </form>
- 
 
-
+                
+            </div>
+            <div class="col-3"></div>
         </div>
-    </div>
-    <div class="col-3"></div>
+        <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6 d-flex justify-content-between align-items-end mt-3">
+                <button type="submit" class="btn btn-primary w-50">Search</button>
+
+
+                <a href="/search">clear</a>
+                <!-- <div class="form-check form-switch">
+                    <input name="advancSearch" class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" onclick="toggleStartDate()">
+                    <label class="form-check-label" for="flexSwitchCheckDefault">Advanced Search</label>
+                </div> -->
+            </div>
+            <div class="col-3"></div>
+        </div>
+                
+       
+        
+        
+    </form>
+ 
+    <!-- <button type="button" onclick="SearchController().redirectToSearch()">click</button> -->
+
     
-</div>
-    
-    <h1 class="mt-5">Search Results</h1>
+    <h1 class="mt-5">{{count($results)}} Search Results</h1>
     @if(count($results) > 0)
         <div class="row">
             <!-- <ul class="ps-0"> -->
